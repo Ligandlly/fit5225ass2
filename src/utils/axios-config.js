c5a967e44e8f05b1cd38a4f6482206080e4bccb2
lly on Mac <liu_lingyuan@icloud.com>
2023-06-07 18:04:11 +0800

    try to enable cors

--- src/utils/axios-config.js
+++ src/utils/axios-config.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://bolnwx1949.execute-api.us-east-1.amazonaws.com/alpha",
+    headers: {
+        'Access-Control-Allow-Origin': "*"
+    }
 });
 
 export default instance;
